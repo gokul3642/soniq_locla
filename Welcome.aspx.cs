b3d804aa395008e171d5822efd87293ac5bad4d1
gokul3642 <gokul3642@gmail.com>
2016-06-27 19:19:26 +0530

    Revert "Commited"
    This reverts commit 1654d6d1476c77c6125be4fbb45f40e1e755b457.

--- Welcome.aspx.cs
+++ Welcome.aspx.cs
@@ -17,23 +17,23 @@ public partial class Welcome : System.Web.UI.Page
         connection.ConnectionString = ConfigurationManager.ConnectionStrings["CON"].ConnectionString;
         if (!IsPostBack)
         {
-            //DataTable dtQuote = new DataTable();
-            //DataSet dsCatRate = new DataSet();
-            ////using (SqlCommand cmd = new SqlCommand("select * from dbo.Category_Input where iQuoteNumber=" + lquoteNumber))
-            //using (SqlCommand cmd = new SqlCommand("Select Top 4 * from [dbo].[vwQuotes] order by [iDate] Desc"))
-            //{
-            //    cmd.CommandType = CommandType.Text;
-            //    cmd.Connection = connection;
-            //    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
-            //    {
+            DataTable dtQuote = new DataTable();
+            DataSet dsCatRate = new DataSet();
+            //using (SqlCommand cmd = new SqlCommand("select * from dbo.Category_Input where iQuoteNumber=" + lquoteNumber))
+            using (SqlCommand cmd = new SqlCommand("Select Top 4 * from [dbo].[vwQuotes] order by [iDate] Desc"))
+            {
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = connection;
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                {
 
-            //        sda.Fill(dsCatRate);
-            //    }
-            //}
-            //DataTable dtCatRate = new DataTable();
-            //dtCatRate = dsCatRate.Tables[0];
-            //latestQuotesTableData.DataSource = dtCatRate;
-            //latestQuotesTableData.DataBind();
+                    sda.Fill(dsCatRate);
+                }
+            }
+            DataTable dtCatRate = new DataTable();
+            dtCatRate = dsCatRate.Tables[0];
+            latestQuotesTableData.DataSource = dtCatRate;
+            latestQuotesTableData.DataBind();
         }
     }
   
